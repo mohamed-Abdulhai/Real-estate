4237abebd954161ce81c02eeca611b7bc5ae77ad
mohamed-Abdulhai <boy105.mb@gmail.com>
2024-01-22 16:38:57 +0200

    Createing client main routes and sever Side main files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Profile from "./components/Profile";
+import Navbar from "./components/Navbar";
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/sign-in" element={<Signin/>}/>
+        <Route path="/sign-up" element={<Signup/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
